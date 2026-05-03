6af9d2862c0cc8efedc97bd69b1a430ca16b2037
17khatri <ajaybavda17@gmail.com>
2026-05-03 11:16:19 +0530

    update capacitor config file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,12 +6,13 @@ const config: CapacitorConfig = {
   appId: 'com.dairymanagement.app',
   appName: 'Dairy Management',
   webDir: 'capacitor-web',
-  server: serverUrl
-    ? {
-        url: serverUrl,
-        androidScheme: 'https',
-      }
-    : undefined,
+  // server: serverUrl
+  //   ? {
+  //       url: serverUrl,
+  //       androidScheme: 'https',
+  //     }
+  //   : undefined,
+  server: { url: 'https://dairy-management-one.vercel.app', cleartext: true },
 };
 
 export default config;
